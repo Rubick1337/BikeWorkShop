815497f0577c43ff635fd6919921ca277024d64d
Rubick1337 <poss1337@gmail.com.>
2024-11-29 00:33:47 +0300

    feat: add ORM database

--- Server/Models/models.js
+++ Server/Models/models.js
@@ -1,238 +1,153 @@
-const sequelize = require("../db");
-const { DataTypes } = require("sequelize");
+const sequelize = require("../db")
+const {DataTypes} = require('sequelize')
 
-const User = sequelize.define("user", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    surname: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-    },
-    role: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-});
+const User = sequelize.define("User", {
+    id:{type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+    email: {type: DataTypes.STRING, allowNull: false},
+    password: {type: DataTypes.STRING, allowNull: false},
+    role: {type: DataTypes.STRING, allowNull: false},
+    adress: {type: DataTypes.STRING, allowNull: false},
+    surname: {type: DataTypes.STRING, allowNull: false},
 
-const Refresh_Token = sequelize.define('refresh_token', {
-    id_user: {type: DataTypes.INTEGER, primaryKey: true, references: { model: User, key: 'id' }},
+})
+const Refresh_Token = sequelize.define("Refresh_token", {
+    id_user: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,references: {model:User, key: "id"}},
     refresh_token: {type: DataTypes.STRING, allowNull: true},
 })
 
-const Model = sequelize.define("model", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-});
+const  TypeService = sequelize.define("TypeService", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const CategoryService = sequelize.define("CategoryService", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const Service = sequelize.define("Service", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_type_service: {type: DataTypes.INTEGER, allowNull: false,references: {model:TypeService, key: "id"}},
+    id_category_service: {type: DataTypes.INTEGER, allowNull: false,references: {model:CategoryService, key: "id"}},
+    name: {type: DataTypes.STRING, allowNull: false},
+    price: {type: DataTypes.DECIMAL, allowNull: false},
+})
+const  TypePart = sequelize.define("TypePart", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const CategoryPart = sequelize.define("CategoryPart", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const Part = sequelize.define("Part", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_type_part: {type: DataTypes.INTEGER, allowNull: false,references: {model:TypePart, key: "id"}},
+    id_category_part: {type: DataTypes.INTEGER, allowNull: false,references: {model:CategoryPart, key: "id"}},
+    name: {type: DataTypes.STRING, allowNull: false},
+    price: {type: DataTypes.DECIMAL, allowNull: false},
+    model: {type: DataTypes.STRING, allowNull: false},
+    brand: {type: DataTypes.STRING, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    inSell: {type: DataTypes.BOOLEAN, allowNull: false},
+})
+const  TypeBike = sequelize.define("TypeBike", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const CategoryBike = sequelize.define("CategoryBike", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    name: {type: DataTypes.STRING, allowNull: false},
+})
+const Bike = sequelize.define("Bike", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_type_bike: {type: DataTypes.INTEGER, allowNull: false,references: {model:TypeBike, key: "id"}},
+    id_category_bike: {type: DataTypes.INTEGER, allowNull: false,references: {model:CategoryBike, key: "id"}},
+    name: {type: DataTypes.STRING, allowNull: false},
+    price: {type: DataTypes.DECIMAL, allowNull: false},
+    model: {type: DataTypes.STRING, allowNull: false},
+    brand: {type: DataTypes.STRING, allowNull: false},
+    img: {type: DataTypes.STRING, allowNull: false},
+    inSell: {type: DataTypes.BOOLEAN, allowNull: false},
+})
+const Basket = sequelize.define("Basket", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_user: {type: DataTypes.INTEGER, allowNull: false,references: {model:User, key: "id"}},
+    cost: {type: DataTypes.DECIMAL, allowNull: false},
+    status: {type: DataTypes.STRING, allowNull: false},
+    date: {type: DataTypes.DATE, allowNull: false},
+})
+const ServiceOrder = sequelize.define("ServiceOrder", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_service: {type: DataTypes.INTEGER, allowNull: false,references: {model:Service, key: "id"}},
+    id_basket: {type: DataTypes.INTEGER, allowNull: false,references: {model:Basket, key: "id"}},
+})
+const PartOrder = sequelize.define("PartOrder", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_service: {type: DataTypes.INTEGER, allowNull: false,references: {model:Part, key: "id"}},
+    id_basket: {type: DataTypes.INTEGER, allowNull: false,references: {model:Basket, key: "id"}},
+})
+const BikeOrder = sequelize.define("BikeOrder", {
+    id: {type: DataTypes.INTEGER, allowNull: false,primaryKey: true,autoIncrement: true},
+    id_service: {type: DataTypes.INTEGER, allowNull: false,references: {model:Bike, key: "id"}},
+    id_basket: {type: DataTypes.INTEGER, allowNull: false,references: {model:Basket, key: "id"}},
+})
 
-const Type = sequelize.define("type", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-});
+User.hasOne(Refresh_Token, {foreignKey: 'id_user',sourceKey: 'id', as: 'refresh_token'});
+Refresh_Token.belongsTo(User, { foreignKey: 'id_user',targetKey: 'id', as: 'user'});
 
-const Category = sequelize.define("category", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-    },
-});
+TypeService.hasMany(Service, {foreignKey: 'id_type_service',sourceKey: 'id', as: 'Service'});
+Service.belongsTo(TypeService, { foreignKey: 'id_type_service',targetKey: 'id', as: 'TypeService' });
 
-const Bike = sequelize.define("bike", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    brand: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-    imgPath: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    inSell: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-});
+CategoryService.hasMany(Service, {foreignKey: 'id_category_service',sourceKey: 'id', as: 'Service'});
+Service.belongsTo(CategoryService, { foreignKey: 'id_category_service',targetKey: 'id', as: 'CategoryService' });
 
-const Service = sequelize.define("service", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    type: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-});
+TypePart.hasMany(Part, {foreignKey: 'id_type_part',sourceKey: 'id', as: 'Part'});
+Part.belongsTo(TypePart, { foreignKey: 'id_type_part',targetKey: 'id', as: 'TypePart' });
 
-const Detail = sequelize.define("detail", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    brand: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.DECIMAL(10, 2),
-        allowNull: false,
-    },
-    imgPath: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-    inSell: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-    },
-});
+CategoryPart.hasMany(Part, {foreignKey: 'id_category_part',sourceKey: 'id', as: 'Part'});
+Part.belongsTo(CategoryPart, { foreignKey: 'id_category_part',targetKey: 'id', as: 'CategoryPart' });
 
-const Basket = sequelize.define("basket", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-});
+TypeBike.hasMany(Bike, {foreignKey: 'id_type_bike',sourceKey: 'id', as: 'Bike'});
+Bike.belongsTo(TypeBike, { foreignKey: 'id_type_bike',targetKey: 'id', as: 'TypeBike' });
 
-const Order = sequelize.define("order", {
-    id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    status: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-    },
-    totalPrice: {
-        type: DataTypes.DECIMAL(10, 2),    allowNull: false,
-    },
-});
+CategoryBike.hasMany(Bike, {foreignKey: 'id_category_bike',sourceKey: 'id', as: 'Bike'});
+Bike.belongsTo(CategoryBike, { foreignKey: 'id_category_bike',targetKey: 'id', as: 'CategoryBike' });
 
+Service.hasMany(ServiceOrder, {foreignKey: 'id_service',sourceKey: 'id', as: 'ServiceOrder'});
+ServiceOrder.belongsTo(Service, { foreignKey: 'id_service',targetKey: 'id', as: 'Service' });
 
-// Связь User и Token (один пользователь может иметь один токенов)
-User.hasOne(Refresh_Token, {foreignKey: 'userId',sourceKey: 'id'});
-Refresh_Token.belongsTo(User, { foreignKey: 'userId',targetKey: 'id'});
+Part.hasMany(PartOrder, {foreignKey: 'id_part',sourceKey: 'id', as: 'PartOrder'});
+PartOrder.belongsTo(Part, { foreignKey: 'id_part',targetKey: 'id', as: 'Part' });
 
-// Связь Model и Bike (одна модель велосипеда может иметь много велосипедов)
-Model.hasMany(Bike, { foreignKey: 'modelId' });
-Bike.belongsTo(Model, { foreignKey: 'modelId' });
+Bike.hasMany(BikeOrder, {foreignKey: 'id_bike',sourceKey: 'id', as: 'BikeOrder'});
+BikeOrder.belongsTo(Bike, { foreignKey: 'id_bike',targetKey: 'id', as: 'Bike' });
 
-// Связь Type и Bike (один тип велосипеда может иметь много велосипедов)
-Type.hasMany(Bike, { foreignKey: 'typeId' });
-Bike.belongsTo(Type, { foreignKey: 'typeId' });
+User.hasMany(Basket, {foreignKey: 'id_user',sourceKey: 'id', as: 'Basket'});
+Basket.belongsTo(User, { foreignKey: 'id_user',targetKey: 'id', as: 'User' });
 
-// Связь Category и Bike (одна категория велосипеда может иметь много велосипедов)
-Category.hasMany(Bike, { foreignKey: 'categoryId' });
-Bike.belongsTo(Category, { foreignKey: 'categoryId' });
+Basket.hasMany(ServiceOrder, {foreignKey: 'id_basket',sourceKey: 'id', as: 'ServiceOrder'});
+ServiceOrder.belongsTo(Basket, { foreignKey: 'id_basket',targetKey: 'id', as: 'Basket' });
 
-// Связь Category и Service (одна категория сервиса может иметь много сервисов)
-Category.hasMany(Service, { foreignKey: 'categoryId' });
-Service.belongsTo(Category, { foreignKey: 'categoryId' });
+Basket.hasMany(PartOrder, {foreignKey: 'id_basket',sourceKey: 'id', as: 'PartOrder'});
+PartOrder.belongsTo(Basket, { foreignKey: 'id_basket',targetKey: 'id', as: 'Basket' });
 
-// Связь Type и Service (один тип сервиса может иметь много сервисов)
-Type.hasMany(Service, { foreignKey: 'typeId' });
-Service.belongsTo(Type, { foreignKey: 'typeId' });
+Basket.hasMany(BikeOrder, {foreignKey: 'id_basket',sourceKey: 'id', as: 'BikeOrder'});
+BikeOrder.belongsTo(Basket, { foreignKey: 'id_basket',targetKey: 'id', as: 'Basket' });
 
-// Связь Model и Detail (одна модель детали может иметь много деталей)
-Model.hasMany(Detail, { foreignKey: 'modelId' });
-Detail.belongsTo(Model, { foreignKey: 'modelId' });
-
-// Связь Type и Detail (один тип детали может иметь много деталей)
-Type.hasMany(Detail, { foreignKey: 'typeId' });
-Detail.belongsTo(Type, { foreignKey: 'typeId' });
-
-// Связь Category и Detail (одна категория детали может иметь много деталей)
-Category.hasMany(Detail, { foreignKey: 'categoryId' });
-Detail.belongsTo(Category, { foreignKey: 'categoryId' });
-
-// Связь User и Basket (один пользователь может иметь много корзин)
-User.hasMany(Basket, { foreignKey: 'userId' });
-Basket.belongsTo(User, { foreignKey: 'userId' });
-
-// Связь Bike и Basket (один велосипед может быть в многих корзинах)
-Bike.hasMany(Basket, { foreignKey: 'bikeId' });
-Basket.belongsTo(Bike, { foreignKey: 'bikeId' });
-
-// Связь Service и Basket (один сервис может быть в многих корзинах)
-Service.hasMany(Basket, { foreignKey: 'serviceId' });
-Basket.belongsTo(Service, { foreignKey: 'serviceId' });
-
-// Связь Detail и Basket (одна деталь может быть в многих корзинах)
-Detail.hasMany(Basket, { foreignKey: 'detailId' });
-Basket.belongsTo(Detail, { foreignKey: 'detailId' });
-
-// Связь Basket и Order (одна корзина может иметь много заказов)
-Basket.hasMany(Order, { foreignKey: 'basketId' });
-Order.belongsTo(Basket, { foreignKey: 'basketId' });
-
-module.exports = { User, Token, Model, Type, Category, Bike, Service, Detail, Basket, Order };
+module.exports = {
+    User,
+    Refresh_Token,
+    Basket,
+    ServiceOrder,
+    PartOrder,
+    BikeOrder,
+    TypeBike,
+    TypePart,
+    TypeService,
+    CategoryPart,
+    CategoryBike,
+    CategoryService,
+    Bike,
+    Part,
+    Service,
+}
--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ require("dotenv").config()
 const express = require("express")
 const sequelize = require("./db")
 const cors = require('cors')
-
+const models = require("./Models/models")
 const PORT = process.env.PORT || 5000
 
 const app = express()
