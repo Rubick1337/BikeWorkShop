ed9c18cebf30486c8fd3fb568ade625f6d83dc27
Rubick1337 <poss1337@gmail.com.>
2024-12-03 20:52:28 +0300

    fix: rework header adding him boostrap and adaptive

--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -1,25 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "./headerStyle.scss"
+import { Navbar, NavbarToggler, Nav, NavItem, Offcanvas, OffcanvasBody, OffcanvasHeader } from 'reactstrap'; // Импортируем компоненты из Bootstrap
+import './headerStyle.scss';
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => setIsOpen(!isOpen);
+
     return (
         <header className="header">
             <div className="header__logo">
                 <div className="logo"></div>
                 <h2 className="Title__text">Bike Workshop</h2>
             </div>
-            <div className="header__menu">
-                <nav className="navigation">
-                    <ul>
-                        <li><Link to="/">Велосипеды</Link></li>
-                        <li><Link to="/about">Услуги</Link></li>
-                        <li><Link to="/services">Контакты</Link></li>
-                        <li><Link to="/contact">Детали</Link></li>
-                        <li><Link to="/">Конструктор</Link></li>
-                    </ul>
-                </nav>
-                <div className="user-actions">
+
+            <Navbar color="light" light expand="md" className="header__navbar">
+                <NavbarToggler onClick={toggle} className="d-md-none" />
+
+                {/* Offcanvas меню для мобильных устройств */}
+                <Offcanvas isOpen={isOpen} toggle={toggle} placement="end" className="offcanvas-menu">
+                    <OffcanvasHeader toggle={toggle}>
+                        <p>Меню</p>
+                    </OffcanvasHeader>
+                    <OffcanvasBody>
+                        <Nav vertical>
+                            <NavItem>
+                                <Link to="/bike" className="nav-link">Велосипеды</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/services" className="nav-link">Услуги</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/contact" className="nav-link">Контакты</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/details" className="nav-link">Детали</Link>
+                            </NavItem>
+                            <NavItem>
+                                <Link to="/constructor" className="nav-link">Конструктор</Link>
+                            </NavItem>
+                            {/* Иконки профиля и корзины внутри бокового меню */}
+                            <div className="user-actions-mobile">
+                                <NavItem>
+                                    <div className="profile__container">
+                                        <div className="profile__background"></div>
+                                    </div>
+                                </NavItem>
+                                <NavItem>
+                                    <div className="basket__container">
+                                        <div className="basket__background"></div>
+                                    </div>
+                                </NavItem>
+                            </div>
+                        </Nav>
+                    </OffcanvasBody>
+                </Offcanvas>
+
+                {/* Навигация для больших экранов */}
+                <Nav className="ml-auto d-none d-md-flex" navbar>
+                    <NavItem>
+                        <Link to="/bike" className="nav-link">Велосипеды</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/services" className="nav-link">Услуги</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/contact" className="nav-link">Контакты</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/details" className="nav-link">Детали</Link>
+                    </NavItem>
+                    <NavItem>
+                        <Link to="/constructor" className="nav-link">Конструктор</Link>
+                    </NavItem>
+                </Nav>
+
+                {/* Иконки профиля и корзины на больших экранах */}
+                <div className="user-actions-desktop">
                     <div className="profile__container">
                         <div className="profile__background"></div>
                     </div>
@@ -27,7 +85,7 @@ const Header = () => {
                         <div className="basket__background"></div>
                     </div>
                 </div>
-            </div>
+            </Navbar>
         </header>
     );
 };
